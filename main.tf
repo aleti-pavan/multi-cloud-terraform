644263c8b2d58ae94c532ae7e174dc984391dbfc
Pavan Aleti <pavan.hpsa@gmail.com>
2020-07-18 21:16:56 +0100

    aws azure gcp storage

--- main.tf
+++ main.tf
@@ -1,16 +1,37 @@
 provider "google" {
   credentials = file("~/.gcloud/credentials/presentation-project-283520-22aa56d34112.json")
-  project     = var.project
-  region      = var.region
+  project     = var.gcp_project
+  region      = var.gcp_region
 }
 
 
 provider "azurerm" {
+
   features {}
 }
 
-provider "aws"{
+provider "aws" {
   profile = "terraform"
-  region = var.region
+  region  = var.aws_region
 }
 
+module "aws" {
+  source      = "./aws"
+  bucket_name = var.bucket_name
+  tags        = var.tags
+}
+
+module "azure" {
+  source      = "./azure"
+  bucket_name = var.bucket_name
+  tags        = var.tags
+  rg          = var.azure_rg
+  location    = var.azure_location
+}
+
+module "gcp" {
+  source      = "./gcp"
+  bucket_name = var.bucket_name
+  project     = var.gcp_project
+  region      = var.gcp_region
+}
